ebabcd08579d717d52f74ef1e350c6c25f240159
jacquem <jacquem@lapp.in2p3.fr>
2015-07-07 16:42:59 +0200

    REmove hessioxxx/include/eventio_version.h

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ OPTIONS="-DCTA -DCTA_MAX_SC -g -O2 -Wall -D_LARGEFILE64_SOURCE -fPIC -DPIC -lm"
 
 echo "build hessio library"
 cd hessioxxx
+mkdir bin out lib
 make  CDEBUGFLAGS="-g -O2" DEFINES="-DCTA -DCTA_MAX_SC"
 cd ..
 
